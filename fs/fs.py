6ad99ba5cfcf5f08a9566e8fcf8abaa9c24a0309
Justin Chadwell <jedevc@gmail.com>
2018-08-13 18:32:10 +0100

    Move router code to seperate file

--- fs/fs.py
+++ fs/fs.py
@@ -1,3 +1,5 @@
+from . import router
+
 def main():
     fs = FileSystem()
     fs.onread('place/here', lambda: 'this is here')
@@ -9,7 +11,7 @@ def main():
 
 class FileSystem:
     def __init__(self):
-        self.router = Router()
+        self.router = router.Router()
 
     def onread(self, route, callback):
         self.router.add(route, callback)
@@ -18,72 +20,3 @@ class FileSystem:
         result = self.router.lookup(route)
         if result:
             return result.data(result.parameters)
-
-class Router:
-    def __init__(self):
-        self.final = None
-
-        self.routes = {}
-        self.vroutes = {}
-
-    def add(self, route, data):
-        if route:
-            try:
-                first, rest = route.split('/', 1)
-            except ValueError:
-                first = route
-                rest = None
-
-            if first.startswith(':'):
-                first = first[1:]
-                if first not in self.vroutes:
-                    self.vroutes[first] = Router()
-                self.vroutes[first].add(rest, data)
-            else:
-                if first not in self.routes:
-                    self.routes[first] = Router()
-                self.routes[first].add(rest, data)
-        else:
-            self.final = data
-
-    def lookup(self, route):
-        result = self.find(route)
-        if result:
-            result.data = result.data.final
-        return result
-
-    def list(self, route):
-        result = self.find(route)
-        if result:
-            keys = result.data.routes.keys()
-            keys = list(keys)
-            result.data = keys
-        return result
-
-    def find(self, route):
-        if route:
-            try:
-                first, rest = route.split('/', 1)
-            except:
-                first = route
-                rest = None
-
-            if first in self.routes:
-                return self.routes[first].find(rest)
-
-            for var in self.vroutes:
-                result = self.vroutes[var].find(rest)
-                if result:
-                    result.parameter(var, first)
-                    return result
-        else:
-            return Result(self)
-
-class Result:
-    def __init__(self, data):
-        self.data = data
-
-        self.parameters = {}
-    
-    def parameter(self, param, data):
-        self.parameters[param] = data
---
+++ fs/router.py
@@ -0,0 +1,68 @@
+class Router:
+    def __init__(self):
+        self.final = None
+
+        self.routes = {}
+        self.vroutes = {}
+
+    def add(self, route, data):
+        if route:
+            try:
+                first, rest = route.split('/', 1)
+            except ValueError:
+                first = route
+                rest = None
+
+            if first.startswith(':'):
+                first = first[1:]
+                if first not in self.vroutes:
+                    self.vroutes[first] = Router()
+                self.vroutes[first].add(rest, data)
+            else:
+                if first not in self.routes:
+                    self.routes[first] = Router()
+                self.routes[first].add(rest, data)
+        else:
+            self.final = data
+
+    def lookup(self, route):
+        result = self.find(route)
+        if result:
+            result.data = result.data.final
+        return result
+
+    def list(self, route):
+        result = self.find(route)
+        if result:
+            keys = result.data.routes.keys()
+            keys = list(keys)
+            result.data = keys
+        return result
+
+    def find(self, route):
+        if route:
+            try:
+                first, rest = route.split('/', 1)
+            except:
+                first = route
+                rest = None
+
+            if first in self.routes:
+                return self.routes[first].find(rest)
+
+            for var in self.vroutes:
+                result = self.vroutes[var].find(rest)
+                if result:
+                    result.parameter(var, first)
+                    return result
+        else:
+            return Result(self)
+
+class Result:
+    def __init__(self, data):
+        self.data = data
+
+        self.parameters = {}
+    
+    def parameter(self, param, data):
+        self.parameters[param] = data
